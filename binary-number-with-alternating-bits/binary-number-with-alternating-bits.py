b0a72aaa022b694af720d9e70d6767392c7e4469
KYUNGHEE YANG <alicesister1@gmail.com>
2021-12-18 22:44:24 +0900

    Time: 24 ms (95.91%), Space: 14.5 MB (6.92%) - LeetHub

---
+++ binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -0,0 +1,34 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        return self.soulition1(n)
+    
+    # 문자열을 2진수 형태로 바꾼 후 직접 비교
+    def soulition1(self, n: int) -> bool:
+        bits = '{:b}'.format(n)
+        for i, bit in enumerate(bits):
+            print(i, bit)
+            if i == len(bits) - 1:
+                break
+                
+            if bits[i] == bits[i + 1]:
+                return False
+        
+        return True
+    
+    # 비트 쉬프팅을 통해 첫 번째 비트만 비교
+    def soulition2(self, n: int) -> bool:
+        # and 연산을 통해 첫 번째(2^0) 비트를 구함.
+        prev_bit = n & 1
+        while n != 1:
+            # 오른쪽 쉬프트 한 n의 첫 번째 비트를 구함
+            n = n >> 1
+            current_bit = n & 1
+            
+            # 현재 첫 번째 비트와 이전 첫 번째 비트를 비교. 다른 경우만 1 출력되는 xor 사용
+            isDiff = current_bit ^ prev_bit
+            if isDiff == 0:
+                return False
+            
+            # 현재 첫 번째 비트를 다음 n의 첫 번째 비트와 비교를 위해 저장
+            prev_bit = current_bit
+        return True 
