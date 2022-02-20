df1e174a44e5081ee88a323ffe40e038019c0762
KYUNGHEE YANG <alicesister1@gmail.com>
2022-02-20 23:59:24 +0900

    Time: 399 ms (78.95%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -2,4 +2,6 @@
 
 SELECT w1.Id 
 FROM Weather w1, Weather w2
-WHERE w1.Temperature > w2.Temperature AND TO_DAYS(w1.recordDate) - TO_DAYS(w2.recordDate) = 1;
+WHERE 
+    TO_DAYS(w1.recordDate) - TO_DAYS(w2.recordDate) = 1 AND
+    w1.Temperature > w2.Temperature;
