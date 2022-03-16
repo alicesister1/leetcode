f8077c3e4ad28de3bbdfc83535995fb88b640ecd
KYUNGHEE YANG <alicesister1@gmail.com>
2022-03-16 23:18:55 +0900

    Time: 3 ms (61.91%), Space: 57.1 MB (9.37%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        int criteria = (int) (nums.length * 0.5 + 0.5);
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.putIfAbsent(n, 0);
-            map.computeIfPresent(n, (integer, integer2) -> integer2 + 1);
-        }
-
-        for (Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() >= criteria) {
-                return e.getKey();
-            }
-        }
-
-        return 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+        return nums[len / 2];
     }
 }
